9705ffcb0443e6bfe22df319e7263a2e4c18a10c
Lauren Ashley <la.zaubi@gmail.com>
2022-10-17 12:11:20 -0700

    added ip fetcher capability usin gipify api

--- iss.js
+++ iss.js
@@ -1,1 +1,39 @@
+// iss.js
 
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+
+const request = require('request');
+
+const fetchMyIP = (callback) => {
+  // use request to fetch IP address from JSON API
+  request(`https://api.ipify.org/?format=json`, (error, response, body) => {
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    // All is good, pass the data along
+    const ip = JSON.parse(body).ip;
+    callback(error, ip);
+
+  });
+};
+
+
+
+module.exports = { fetchMyIP };
